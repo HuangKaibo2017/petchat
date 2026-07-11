b34dc2f19bb149a6f72d40a658440f3862b7936b
曼因酪福. <wangpengyuyz@gmail.com>
2026-07-11 12:32:35 +1000

    feat: restore shop demo pages with product data and styles

--- wechat/pages/shop/shop.js
+++ wechat/pages/shop/shop.js
@@ -1,37 +1,24 @@
-const app = getApp()
+const { SHOP_ITEMS, getProductsByCategory } = require('./products')
 
 Page({
   data: {
-    products: [],
+    products: SHOP_ITEMS,
     categories: [
       { key: 'all', name: '全部' },
-      { key: 'nfc', name: '智能设备' },
-      { key: 'beads', name: '香珠' },
-      { key: 'food', name: '食品' },
-      { key: 'insurance', name: '保险' }
+      { key: 'handmade', name: '手作' },
+      { key: 'necklace', name: '项链' },
+      { key: 'toy', name: '玩具' }
     ],
     activeCategory: 'all',
-    loading: true
-  },
-
-  onLoad() {
-    this.loadProducts()
-  },
-
-  loadProducts() {
-    this.setData({ loading: true })
-    const { MockAPI } = require('../../utils/mock')
-    MockAPI.getProducts().then(res => {
-      const products = (res && res.data) ? res.data : []
-      this.setData({ products, loading: false })
-    }).catch(() => {
-      this.setData({ loading: false })
-    })
+    loading: false
   },
 
   switchCategory(e) {
     const key = e.currentTarget.dataset.key
-    this.setData({ activeCategory: key })
+    this.setData({
+      activeCategory: key,
+      products: getProductsByCategory(key)
+    })
   },
 
   goDetail(e) {
